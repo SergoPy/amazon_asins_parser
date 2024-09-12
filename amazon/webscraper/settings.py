8f8ddd344286715ff5f6299b2073e5df608250f8
SergoPy <shchokotov22@gmail.com>
2024-09-12 20:11:40 +0300

    Change media endpoints

--- amazon/webscraper/settings.py
+++ amazon/webscraper/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'v2/static/'
-STATIC_ROOT = 'v2/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'v2/media/')
-MEDIA_URL = 'v2/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
